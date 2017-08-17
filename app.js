b7de0ca48a963de74a0dcf9c99566f44e8f925e8
Colton Dowling <Colton@cd3style.com>
2017-08-17 16:27:33 -0500

    working good

---
+++ app.js
@@ -0,0 +1,61 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const expressValidator = require('express-validator');
+const mustacheExpress = require('mustache-express');
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(expressValidator());
+
+app.use(function (req,res,next){
+console.log("in interceptor");
+  next();
+})
+
+app.get('/', function(req,res) {
+  res.render('login')
+})
+
+app.post('/login', function (req, res) {
+  console.log("username is" + req.body.undername);
+  console.log("password is" + req.body.password);
+  res.render('home')
+
+})
+
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+//Listening on root
+app.get('/', function(req, res) {
+  res.render('home', {})
+})
+app.get('/', function(req, res){
+  res.send('I am alive')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("youre entering the first part friend");
+})
