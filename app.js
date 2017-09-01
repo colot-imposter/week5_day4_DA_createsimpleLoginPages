46e83485cf0a1d083f941ebcfe7a9967e2c93374
Colton Dowling <Colton@cd3style.com>
2017-08-31 21:07:59 -0500

    added a log out button

--- app.js
+++ app.js
@@ -1,81 +1,67 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const expressValidator = require('express-validator');
-const mustacheExpress = require('mustache-express');
-const session = require('express-session')
+console.log('Entering the Web(world-wider than you thought. its big...)');
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const mustacheExpress = require('mustache-express');
+const session = require('express-session');
 const app = express();
 
-const username_password = [{
-    user: 'colot',
-    pass: 'the'
-  },
-  {
-    user: 'helmut',
-    pass: 'aufs'
-  },
-  {
-    user: 'den',
-    pass: 'hause'
-  }
-
-]
-
-app.engine('mustache', mustacheExpress())
-app.set('views', './views')
-app.set('view engine', 'mustache')
+const logins = [{username:'colot', password:'the'}, {username:'hans', password:'burkel'}];
 
 app.use(bodyParser.urlencoded({
   extended: false
-}));
-app.use(expressValidator());
+}))
+
+app.set('trust proxy', 1)
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
-}));
+}))
 
-app.use((req, res, next) => {
-  if (req.url === '/login') {
-    next();
+app.engine('mustache', mustacheExpress());
+
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.use(function(req, res, next) {
+  console.log('in interceptor');
+  if (req.url == '/login') {
+    next()
   } else if (!req.session.login) {
-    res.render('login');
+    res.render('login')
   } else {
-    next();
+    next()
   }
-});
+})
 
 app.get('/', function(req, res) {
   res.render('home')
 })
 
 app.post('/login', function(req, res) {
-  console.log("start of homer");
-  username_password.forEach(function(e) {
-    console.log("req.session.login", req.session.login);
-    console.log("e.user", e.user);
-    console.log("req.body.username", req.body.username);
-    console.log("e.pass", e.pass);
-    console.log("req.body.password", req.body.password);
-    if (e.user === req.body.username && e.pass === req.body.password) {
+  console.log("username is " + req.body.username);
+  console.log("password is " + req.body.password);
+  for (let i = 0; i < logins.length; i++) {
+    if (req.body.username === logins[i].username && req.body.password === logins[i].password) {
       req.session.login = req.body.username;
-      console.log("home is close");
+      console.log('hello');
     }
-    if (req.session.login === req.body.username) {
-      console.log(req.session.login);
-      res.render('home')
-    } else {
-      res.render('login', {
-        oops: true
-      })
-    }
-  })
+  }
+  if (req.session.login === req.body.username){
+    res.render('home')
+  }
+  else{
+    res.render('login', {oops:true})
+  }
+
 })
 
-app.post('/logOut', function(req, res) {
-  res.render('login')
+app.post('/logoutBttn', function(req, res){
+  req.session.login = "";
+  res.redirect('/')
 })
 
 app.listen(3000, function() {
-  console.log("youre entering the first part friend");
-})
+  console.log('Entering: Whoop! Listening on port 3000!');
+});
