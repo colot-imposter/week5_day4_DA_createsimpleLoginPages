022e9edbca7ce673e0c6a54373394d321694b43f
Colton Dowling <Colton@cd3style.com>
2017-08-31 20:56:22 -0500

    home working. logout not working. login error not working

--- app.js
+++ app.js
@@ -6,72 +6,76 @@ const session = require('express-session')
 
 const app = express();
 
-app.use(bodyParser.json())
+const username_password = [{
+    user: 'colot',
+    pass: 'the'
+  },
+  {
+    user: 'helmut',
+    pass: 'aufs'
+  },
+  {
+    user: 'den',
+    pass: 'hause'
+  }
 
-app.use(bodyParser.urlencoded({extended: false}));
+]
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(expressValidator());
-
-
-
-app.get('/', function(req,res) {
-  res.render('home')
-})
-
 app.use(session({
-  secret: "whomp",
+  secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
-}))
+}));
 
-const username_password =[]
+app.use((req, res, next) => {
+  if (req.url === '/login') {
+    next();
+  } else if (!req.session.login) {
+    res.render('login');
+  } else {
+    next();
+  }
+});
 
-app.use(function(req,res, next){
-  console.log("in interceptor");
-
-  // if req.url == '/login'
-     //     next()
-     // else if !req.session.login
-     //     res.render('login')
-     // else
-     //     next()
-     next();
-})
-
-app.post('/login', function (req, res) {
-  console.log("username is" + req.body.undername);
-  console.log("password is" + req.body.password);
-  res.render('home')
-
-})
-
-//Listening on root
 app.get('/', function(req, res) {
-  res.render('home', {})
-})
-app.get('/', function(req, res){
-  res.send('I am alive')
+  res.render('home')
 })
 
+app.post('/login', function(req, res) {
+  console.log("start of homer");
+  username_password.forEach(function(e) {
+    console.log("req.session.login", req.session.login);
+    console.log("e.user", e.user);
+    console.log("req.body.username", req.body.username);
+    console.log("e.pass", e.pass);
+    console.log("req.body.password", req.body.password);
+    if (e.user === req.body.username && e.pass === req.body.password) {
+      req.session.login = req.body.username;
+      console.log("home is close");
+    }
+    if (req.session.login === req.body.username) {
+      console.log(req.session.login);
+      res.render('home')
+    } else {
+      res.render('login', {
+        oops: true
+      })
+    }
+  })
+})
 
+app.post('/logOut', function(req, res) {
+  res.render('login')
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("youre entering the first part friend");
 })
